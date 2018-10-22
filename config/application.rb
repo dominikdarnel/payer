84c1304477bb29b94f56a34dde3ea7c1c5d956c9
dominikdarnel <dominik.darnel@gmail.com>
2018-10-22 10:18:32 +0200

    Adding bootstrap

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Payer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+
+      g.view_specs false
+      g.helper_specs false
+    end
   end
 end
