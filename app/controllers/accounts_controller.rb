eac669ae4e51fa57e557a4e07018729d65fdc2c3
Unknown <dominik.darnel@gmail.com>
2018-10-24 11:26:08 +0200

    Accounts design prototype

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,18 +1,10 @@
 class AccountsController < ApplicationController
-  before_action :set_account, only: [:show, :edit, :update, :destroy]
+  before_action :set_account, only: [:edit, :update, :destroy]
 
-  # GET /accounts
-  # GET /accounts.json
   def index
     @accounts = Account.all
   end
 
-  # GET /accounts/1
-  # GET /accounts/1.json
-  def show
-  end
-
-  # GET /accounts/new
   def new
     @account = Presenters::Account.new(Account.new)
   end
@@ -21,15 +13,13 @@ class AccountsController < ApplicationController
   def edit
   end
 
-  # POST /accounts
-  # POST /accounts.json
   def create
     @account_service = Services::AccountModifier.new(Account.new, account_params, current_user)
     @account = @account_service.account
     respond_to do |format|
       if @account_service.save
-        format.html { redirect_to @account, notice: 'Account was successfully created.' }
-        format.json { render :show, status: :created, location: @account }
+        format.html { redirect_to accounts_path, notice: 'Account was successfully created.' }
+        format.json { render :index, status: :created, location: @account }
       else
         format.html { render :new }
         format.json { render json: @account.errors, status: :unprocessable_entity }
@@ -37,24 +27,20 @@ class AccountsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /accounts/1
-  # PATCH/PUT /accounts/1.json
   def update
     @account_service = Services::AccountModifier.new(@account, account_params, current_user)
     @account = @account_service.account
     respond_to do |format|
       if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { render :show, status: :ok, location: @account }
+        format.html { redirect_to accounts_path, notice: 'Account was successfully updated.' }
+        format.json { render :index, status: :ok, location: @account }
       else
-        format.html { render :edit }
+        format.html { render :index }
         format.json { render json: @account.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /accounts/1
-  # DELETE /accounts/1.json
   def destroy
     @account.destroy
     respond_to do |format|
@@ -64,13 +50,12 @@ class AccountsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_account
-      @account = Account.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def account_params
-      params.require(:account).permit(:name, :currency, :amount, :user_id)
-    end
+  def set_account
+    @account = Account.find(params[:id])
+  end
+
+  def account_params
+    params.require(:account).permit(:name, :currency, :amount, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :transactions
-  resources :accounts
+  resources :accounts, except: :show
   resources :cards
   devise_for :users
   root to: "home#home"
