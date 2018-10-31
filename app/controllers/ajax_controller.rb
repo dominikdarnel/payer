80ff73fb37bf56130aab84911e9b110f81875c38
Unknown <dominik.darnel@gmail.com>
2018-10-31 10:32:09 +0100

    JS form for new Transaction WIP

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -4,4 +4,12 @@ class AjaxController < ApplicationController
     cards_for_account = Presenters::Funds.new(current_user).cards_for_account_json(account)
     render json: { result: cards_for_account }
   end
+
+  def accounts_for_transaction
+    account = Account.find(params[:account_id])
+    new_transaction = Transaction.new(from_user: current_user)
+    accounts_for_transaction = Presenters::Transaction.new(new_transaction)
+                                               .accounts_for_transaction_json(current_user, account)
+    render json: { result: accounts_for_transaction }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'landing#index'
+  devise_for :users
   resources :transactions, only: %i[index new create]
   resources :cards, only: %i[index new create destroy]
-  devise_for :users
   resources :accounts, except: :show
   get 'accounts/add_funds' => 'accounts#add_funds'
   post 'accounts/add_funds' => 'accounts#add_funds'
   post 'accounts/cards_for_account' => 'ajax#cards_for_account'
+  post 'transactions/accounts_for_transaction' => 'ajax#accounts_for_transaction'
 end
